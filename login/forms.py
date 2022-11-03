e6b5fd9c9228ad0450ebbe41f5af6b42ee7ed0db
Alan69 <gameg1676@gmail.com>
2022-11-03 17:56:06 +0600

    asd

--- login/forms.py
+++ login/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.forms import ModelForm
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.models import User
 from userconf.models import User
 
 class CustomUserCreationForm(UserCreationForm):
