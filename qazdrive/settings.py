916159aa82513f8b63c37cc6b9e6a3a36bb5846e
Alan69 <gameg1676@gmail.com>
2022-11-03 14:49:48 +0600

    auth

--- qazdrive/settings.py
+++ qazdrive/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'login',
 ]
 
 MIDDLEWARE = [
@@ -117,10 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR/"static"
-# STATICFILES_DIRS=[
-#     BASE_DIR / 'static',
-# ]
+STATICFILES_DIRS=[
+    BASE_DIR / 'static',
+]
 
 MEDIA_ROOT = BASE_DIR / 'media' # media directory in the root directory
 MEDIA_URL = '/media/'
