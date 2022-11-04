b7ab6fd422acfcc24c856f2b4fc97062946779dc
Alan69 <gameg1676@gmail.com>
2022-11-04 12:45:50 +0600

    new register page

--- login/forms.py
+++ login/forms.py
@@ -5,11 +5,12 @@ from django.contrib.auth.forms import UserCreationForm
 from userconf.models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
     class Meta:
         model=User
         fields=['email', 'first_name', 'last_name', 'city']
         widgets = {
-        'email': forms.TextInput(attrs={'class':'form-control'}),
+        'email': forms.EmailInput(attrs={'class':'form-control'}),
         'first_name': forms.TextInput(attrs={'class':'form-control'}),
         'last_name': forms.TextInput(attrs={'class':'form-control'}),
         'city': forms.Select(attrs={'class':'form-control'}),
