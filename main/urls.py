060f8adfc7b4c72bca79d5f51bcaf7805a86c9d8
Alan69 <gameg1676@gmail.com>
2022-11-07 11:35:28 +0600

    profile change

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, profile
+from .views import index, profile, updaterecord
 
 
 urlpatterns = [
     path('', index, name='index'),
     path('profile/', profile, name='profile'),
+    path('profile/updaterecord/<int:id>', updaterecord, name='updaterecord'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.conf import settings
 from userconf.models import User
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
@@ -12,3 +14,16 @@ def profile(request):
         "obj":obj,
     }
     return render(request, 'main/profile.html', context)
+
+def updaterecord(request, id):
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    city = request.POST['city']
+    email = request.POST['email']
+    user = User.objects.get(id=id)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.city = city
+    user.email = email
+    user.save()
+    return HttpResponseRedirect(reverse('profile'))
