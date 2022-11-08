fcf6e2649e8d80869b6cccf42ae72ba63637c6b7
Alan69 <gameg1676@gmail.com>
2022-11-08 17:12:43 +0600

    check order3

--- tarif/models.py
+++ tarif/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Tarif(models.Model):
     name = models.CharField(max_length=255)
     price = models.CharField(max_length=255)
-    img = models.ImageField()
-    icon = models.ImageField()
+    amount = models.IntegerField(verbose_name="Срок тарифа")
 
     def __str__(self):
         return self.name
