3108e3b73c64423f51781afc75b929cbec9d9f36
Alan <45432923+Alan69@users.noreply.github.com>
2024-04-25 14:58:28 +0600

    Update settings_prod.py

--- qazdrive/settings_prod.py
+++ qazdrive/settings_prod.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mvm$o(+)gc%j@&s_i&sej)+k#1zz0t!f9ufx8r^+c4cj(i85eo'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
