c6cd6cbc9808d4eaf12a84a9cfd0ce65f9f376c4
Alan69 <gameg1676@gmail.com>
2025-04-29 14:13:53 +0500

    fix

--- main/views.py
+++ main/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from quiz.models import Result
 
 # Create your views here.
+@login_required(login_url='login')
 def index(request):
-    result = Result.objects.filter(user = request.user)
+    result = Result.objects.filter(user=request.user)
     context = {
-        'result':result
+        'result': result
     }
     return render(request, 'main/index.html', context=context)
     
